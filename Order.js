613a6c7fa15d9c77a4d6ce3969aa8074cc24c185
benorg <benorg@me.com>
2017-03-19 21:24:03 -0400

    electron

--- Order.js
+++ Order.js
@@ -1,63 +1,11 @@
-"use strict";
 class Order {
-	constructor(items, settings) {
-		this._items = items;
-		this._settings = settings;
+	constructor () {
+		this.items;
 	}
 
-	addItem(i) {
-		items.push(i);
-	}
+	addItem() {
 
-	clearItems() {
-		this._items = {};
-	}
-
-
-	clearSettings() {
-		this._settings = {};
-	}
-
-	get items() {
-		return this._items;
-	}
-
-	get settings() {
-		return this._settings;
-	}
-
-	static getMagnitude(a, b) {
-		return Math.pow(a * a + b * b, 0.5);
 	}
 }
 
-class SiteOrder extends Order{
-	constructor() {
-		super('site', 'site');
-	}
-}
-
-console.log(Order.getMagnitude(3, 4));
-
-var o = new Order({i: 'jib'}, {settings: 'blah'});
-
-var s = new SiteOrder();
-
-console.log(SiteOrder.getMagnitude(3, 4));
-console.log(s.items);
-
-var tes = 'Hi';
-
-var obj = {
-	name: 'Blue',
-	tes,
-	age: '15'
-}
-
-
-console.log(o.items);
-console.log(o);
-
-o.clearSettings();
-
-console.log(o.settings);
+module.exports.Order = Order;
---
+++ main.js
@@ -0,0 +1,30 @@
+const electron = require('electron'),
+	app = electron.app,
+	BrowserWindow = electron.BrowserWindow,
+	path = require('path'),
+	url = require('url');
+
+let win; //store the window object
+
+app.on('ready', createWindow);
+
+app.on('windows-all-closed', () => {
+	app.quit(); //you could stay open on mac, but who cares
+});
+
+function createWindow() {
+	win = new BrowserWindow({width: 800, height: 600});
+
+	win.webContents.openDevTools();
+
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+
+	win.on('closed', () => {
+		win = null; //garbage collect the window
+	});
+}
